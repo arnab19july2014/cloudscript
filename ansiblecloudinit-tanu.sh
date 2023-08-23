465b989cb1700c512b310ec93db76829479e009c
Arpan Mandal <arpan.rec@gmail.com>
2023-08-23 14:33:57 +0530

    removed resolvconf

--- ansiblecloudinit-tanu.sh
+++ ansiblecloudinit-tanu.sh
@@ -31,7 +31,7 @@ sudo apt upgrade -y
 
 sudo apt install -y \
   zip unzip net-tools build-essential tar wget curl ca-certificates sudo \
-  resolvconf systemd telnet gnupg2 apt-transport-https lsb-release software-properties-common \
+  systemd telnet gnupg2 apt-transport-https lsb-release software-properties-common \
   locales systemd-timesyncd network-manager gnupg2 gnupg pigz cron acl \
   ufw vim python3-venv git fontconfig gtk-update-icon-cache libnss3 libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 \
   bzip2 libgbm-dev libglib2.0-dev libdrm-dev libasound2 jq zsh libcap2-bin ntfs-3g exfat-fuse vim neovim \
